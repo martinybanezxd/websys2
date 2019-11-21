595d02240393d2b2655f7b3e4405c826a28ec1a3
martinybanezxd <57779413+martinybanezxd@users.noreply.github.com>
2019-11-21 10:14:22 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,26 +3,17 @@
 <head>
 	<style>
 		
-	a{
-	font-family: sans-serif;
-	size: 20px;
-	color: red;
-	}
-
+	
 	</style>
 	<title>Welcome</title>
 </head>
-<body bgcolor="lavender">
-	<?php echo "My name is Reymartin Ybanez.";?> <br>
-	<a href="index.php"> Go to Main Page. </a>
-
-</body>
-</body>
-</body>
-</body>
-</body>
-</body>
-</body>
-</body>
+<body bgcolor="#00CCCC">
+<center><h1 style="font-family: sans-serif; font-weight:500; color: #CC0033">
+	<?php echo "WELCOME!";?> <br><br></h1>
+	<h1>
+	<?php echo "My name is Reymartin Ybanez.";?> <br><br>
+	<a href="index.php" style="color: #996699"> Go to Main Page. </a>
+	<h1>
+</center>
 </body>
 </html>
